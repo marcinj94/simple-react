0d0e4de6a2d28a7e7fecd551d5f05d872f1ee645
beardofdoom <marcin.jusiak@gmail.com>
2019-01-23 23:43:17 +0100

    Create first simple react-app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+
+const persons = [
+  {
+    id: 1,
+    name: "Courtney Kalina"
+  },
+  {
+    id: 2,
+    name: "Delmer Paquet"
+  },
+  {
+    id: 3,
+    name: "Teofila Reinsch",
+  },
+  {
+    id: 4,
+    name: "Tiera Coto",
+  },
+  {
+    id: 5,
+    name: "Phuong Arbuckle",
+  },
+];
+
+
+const UsersList = (props) => {
+
+  const persons = props.persons.map(person => (
+    <li key={person.id}>{person.name}</li>
+  ))
+
+  if (persons.length > 0) {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <ul className="list">
+        {persons}
+      </ul>
+    )
+  }
+  return (
+    <p>No results!</p>
+  )
+}
+
+
+class App extends Component {
+
+  state = {
+    filteredUsers: persons
+  }
+
+
+  filterUsers = (e) => {
+
+    const value = e.target.value;
+
+    const filteredUsers = persons.filter(person => person.name.toLowerCase().includes(value.toLowerCase()));
+
+    this.setState({
+      filteredUsers
+    })
+  }
+
+  render() {
+
+    return (
+      <div className="app" >
+        <h1 className="title">Simple React app</h1>
+        <input onChange={this.filterUsers} className="input" type="text" placeholder="Wpisz imię" />
+        <UsersList persons={this.state.filteredUsers} />
       </div>
-    );
+    )
   }
 }
 
