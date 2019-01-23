c028fd5739a06dc3093e97f92b5db3c1913e2156
beardofdoom <marcin.jusiak@gmail.com>
2019-01-23 23:51:39 +0100

    Updates

---
+++ precache-manifest.81d267f4fbab5fb0c1d5b226c0c705ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b32f1e6352d11702f8c",
+    "url": "/simple-react/static/js/runtime~main.3b32f1e6.js"
+  },
+  {
+    "revision": "db33b8b3da34090fbe6b",
+    "url": "/simple-react/static/js/main.db33b8b3.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/simple-react/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "db33b8b3da34090fbe6b",
+    "url": "/simple-react/static/css/main.ee4a193f.chunk.css"
+  },
+  {
+    "revision": "a1b062c49b8e0b9fffc157ebe8881107",
+    "url": "/simple-react/index.html"
+  }
+];
